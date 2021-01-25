9162a37846761dd63d5ea6e7bcbe1dc506ae197e
ZandroidX <macy.gm@gmail.com>
2021-01-25 00:17:28 -0700

    Song Title Extraction Complete
    Song title now is extracted from file

--- ByteParser.py
+++ ByteParser.py
@@ -2,6 +2,7 @@ import os
 import platform
 from tkinter import filedialog
 import tkinter as tk
+from array import array
 
 root = tk.Tk()
 root.attributes('-topmost', 1)
@@ -10,14 +11,13 @@ root.withdraw()
 
 system = platform.system()
 
-
 def file1():
     if system != 'Windows':
         global filePath1
         filePath1 = filedialog.askopenfilename(initialdir="~/", title="Select File")
     else:
-        filePath1 = filedialog.askopenfilename(initialdir=os.environ['USERPROFILE'], title="Select File")
-
+        filePath1 = filedialog.askopenfilename(initialdir=os.environ['USERPROFILE'] + "\\Downloads\\",
+                                                   title="Select File")
 
 def file2():
     if system != 'Windows':
@@ -26,12 +26,48 @@ def file2():
     else:
         filePath2 = filedialog.askopenfilename(initialdir="C:\\Users\\%USERPROFILE%\\", title="Select File")
 
+
 def file3():
     if system != 'Windows':
         global filePath3
         filePath3 = filedialog.askopenfilename(initialdir="~/", title="Select File")
     else:
         filePath3 = filedialog.askopenfilename(initialdir="C:\\Users\\%USERPROFILE%\\", title="Select File")
+
+
+def gettitle(parsedFile):
+    global songTitleIndex
+    global songTitleArray
+    global songTitleName
+
+    songTitleArray = []
+    songTitleName = ""
+    for s in range(len(parsedFile)):
+        if str(parsedFile[s].hex()) == "ff":
+            if str(parsedFile[s + 1].hex()) == "fe":
+                songTitleIndex = s + 2
+                for m in range(songTitleIndex, len(parsedFile)):
+                    if parsedFile[m] != b'\x00':
+                        if parsedFile[m] == b'\x54':
+                            break
+                        songTitleArray.append(parsedFile[m].decode(encoding="ascii"))
+
+            break
+    print("Extraction complete, displaying song information...")
+    if(len(songTitleArray) > 0):
+        songName = songTitleName.join(songTitleArray)
+        print("Song Title:" + songName)
+    else:
+        print("Format not recognized or cannot find ID3 tag information")
+    # if not songTitleArray:
+    #     print("Could not find song title")
+    #
+    # if songTitleArray:
+    #     songName = songTitleName.join(songTitleArray)
+    #     print("Song title: " + songName)
+
+    exit(0)
+
 
 def compare():
     """"This function returns the SHA-1 hash
@@ -49,36 +85,81 @@ def compare():
             # Do stuff with byte.
             byte = f.read(1)
             parsedFile.append(byte)
-    print("File 1 Successfully Parsed, Size of File: ")
-    print(len(parsedFile))
-    with open(filePath2, 'rb') as f:
-        global parsedFile2
-        parsedFile2 = []
-        byte = f.read(1)
-        parsedFile2.append(byte)
-        while byte:
-            byte = f.read(1)
-            parsedFile2.append(byte)
-    print("File 2 Successfully Parsed, Size of File: ")
-    print(len(parsedFile2))
+        if parsedFile[0] == b'I':
+            if parsedFile[1] == b'D':
+                if parsedFile[2] == b'3':
+                    print("ID3 found, checking Version...")
+                    id3 = []
+                    id3str = ""
+                    s = 0
+                    for s in range(3):
+                        id3.append(parsedFile[s].decode(encoding="ascii"))
+                    print(id3str.join(id3))
 
-    with open(filePath3, 'rb') as f:
-        global parsedFile3
-        parsedFile3 = []
-        byte = f.read(1)
-        parsedFile3.append(byte)
-        while byte:
-            byte = f.read(1)
-            parsedFile3.append(byte)
-    print("File 3 Successfully Parsed, Size of File: ")
-    print(len(parsedFile3))
+                if str(parsedFile[3]).__contains__("x03"):
+                    print("ID3v2.3 Identified, Processing Metadata....")
+                    if str(parsedFile[10]) == str(b'T'):
+                        print("Found Title!")
+                        for s in range(len(parsedFile)):
+                            if parsedFile[s] == b'T':
+                                global titleStart
+                                titleStart = s
+                                if parsedFile[s + 1] == b'I':
+                                    print("First Title Index: " + str(titleStart))
+                                    break
 
-    for b in range(len(parsedFile)):
-        if b < len(parsedFile):
-            if b < len(parsedFile2):
-                if parsedFile[b] == parsedFile2[b]:
-                    if parsedFile[b] == parsedFile3[b]:
-                        print("Match Found at byte location: " + str(b) + str(parsedFile[b]))
+                        for s in range(len(parsedFile)):
+                            if parsedFile[s] == b'T':
+                                if parsedFile[s + 1] == b'P':
+                                    if parsedFile[s + 2] == b'E':
+                                        if parsedFile[s + 3] == b'1':
+                                            global ArtistInfo
+                                            ArtistInfo = s
+                                            print("Artist Tag at index: " + str(ArtistInfo))
+                                    break
+
+                        for s in range(len(parsedFile)):
+                            if parsedFile[s] == b'T':
+                                if parsedFile[s + 1] == b'A':
+                                    if parsedFile[s + 2] == b'L':
+                                        if parsedFile[s + 3] == b'B':
+                                            global Album
+                                            Album = s
+                                            print("Album Tag at index: " + str(Album))
+
+        gettitle(parsedFile=parsedFile)
+
+    # print("File 1 Successfully Parsed, Size of File: ")
+    # print(len(parsedFile))
+    # with open(filePath2, 'rb') as f:
+    #     global parsedFile2
+    #     parsedFile2 = []
+    #     byte = f.read(1)
+    #     parsedFile2.append(byte)
+    #     while byte:
+    #         byte = f.read(1)
+    #         parsedFile2.append(byte)
+    # print("File 2 Successfully Parsed, Size of File: ")
+    # print(len(parsedFile2))
+    #
+    # with open(filePath3, 'rb') as f:
+    #     global parsedFile3
+    #     parsedFile3 = []
+    #     byte = f.read(1)
+    #     parsedFile3.append(byte)
+    #     while byte:
+    #         byte = f.read(1)
+    #         parsedFile3.append(byte)
+    # print("File 3 Successfully Parsed, Size of File: ")
+    # print(len(parsedFile3))
+    #
+    # for b in range(len(parsedFile)):
+    #     if b < len(parsedFile):
+    #         if b < len(parsedFile2):
+    #             if parsedFile[b] == parsedFile2[b]:
+    #                 if parsedFile[b] == parsedFile3[b]:
+    #                     print("Match Found at byte location: " + str(b) + str(parsedFile[b]))
+
 
 root.deiconify()
 root.after_idle(root.attributes, '-topmost', 0)
